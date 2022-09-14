e88267c1c8479b9abc099f46de60c744d5b5e8af
tarolong2 <tarolong2@gmail.com>
2022-09-14 10:12:12 +0900

    최초생성

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,24 @@
-import {createRouter, createWebHistory} from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import DetailView from '../views/DetailView.vue'
 const router = createRouter({
   history: createWebHistory(),
-  routes: [
+  routes:[
     {
-      path: '/',
-      redirect: '/home'
+      path:'/page-ghibli/home',
+      redirect:'/'
     },
     {
-      // 웹브라우저 주소 표시줄에 명시
-      path: '/home',
-      // RouterView 에 보여줄 페이지 컴포넌트 이름
-      component: HomeView
+    // 웹브라우저 주소 표시줄에 명시
+    path: '/page-ghibli/',
+    // RouterView 에 보여줄 페이지 컴포넌트 이름
+    component: HomeView
     },
+
     {
-      path: '/detail/:id',
+      path: '/page-ghibli/detail/:id',
       component: DetailView
     }
   ]
 });
-
 export default router
